0ee94c1a470232f1d937bd129cc90125dbf69517
xNECROMANCERx <hakeemhamza147@gmail.com>
2023-09-25 12:51:57 +0530

    Incorporated numerous new functionalities and choices.

--- passswordgen.py
+++ passswordgen.py
@@ -3,6 +3,7 @@ import string
 
 
 def pass1(m):
+    #all type of characters
     letters_=string.ascii_letters
     digits_=string.digits
     uppercase_=random.choice(string.ascii_uppercase)
@@ -21,6 +22,7 @@ def pass1(m):
     print(passwordstr)
     return
 def pass2(n):
+    #only letters,digits and uppercase
     letters_=string.ascii_letters
     digits_=string.digits
     password=[random.choice(string.ascii_uppercase)]
@@ -31,7 +33,15 @@ def pass2(n):
     passwordstr="".join(password)
     print(passwordstr)
     return
+def pass7(l):
+    #only lowercase
+    password=[random.sample(string.ascii_lowercase,l)]
+    passwordstr=''.join(password)
+    print(passwordstr)
+    return
+
 def pass3(o):
+    #only letters and uppercase
     letters_=string.ascii_letters
     password=[random.choice(string.ascii_uppercase)]
     length_left= int(l) -1
@@ -42,31 +52,92 @@ def pass3(o):
     passwordstr="".join(password)
     print(passwordstr)
     return
+def pass6(v):
+    #only letters and numbers
+    letters_=string.ascii_letters
+    digits_=string.digits
+    mix=[letters_+digits_]
+
+    password=random.sample(mix,v)
+    password.str="".join(password)
+    print(passwordstr)
+    return
+
+def pass4(p):
+    #only letters and special characters
+    letters_=string.ascii_letters
+    special_=random.choice(["!","@",'+','=','-','_','#','$','%','^','&','*'])
+    password=[special_]
+    lengthleft=int(p) - 1
+    d=random.sample(letters_,lengthleft)
+    password.extend(d)
+    passwordstr=''.join(password)
+    print(passwordstr)
+    return
+
+def pass5(l):
+    #only letters
+    password=random.sample((string.ascii_letters),l)
+    passwordstr="".join(password)
+    print(passwordstr)
+    return
+def pass8(x):
+    #letters, numbers and special charcs
+    letters_=string.ascii_letters
+    special_=random.choice(["!","@",'+','=','-','_','#','$','%','^','&','*'])
+    digits_=string.digits
+    strin=[letters_,digits_]
+    password=[random.choice(special_)]
+    lengthleft= int(x) - 1
+    strin2=random.sample(strin,lengthleft)
+    password.extend(strin2)
+    passwordstr="".join(password)
+    print(passwordstr)
+    return
+
 while True:
+    print("Password Generator")
     l=int(input("How long do you want the password to be? (Enter a digit between 4-20)\n"))
     if l>=4 and l<=20:
-        print("Password Generator")
-        a=input("Do you want to include Uppercase alphabets? \n")
-        if a=="yes":
-            b=input("Do you want to include numbers?\n")
-            if b=="yes":
-                c=input("Do you want to include special character?\n")
-                if c=="yes":
+        
+        a=str.lower(input("Do you want to include Uppercase alphabets? \n"))
+        if a==("yes" or "y"):
+            b=str.lower(input("Do you want to include numbers?\n"))
+            if b==("yes" or "y"):
+                c=str.lower(input("Do you want to include special characters?\n"))
+                if c==("yes" or"y"):
                     pass1(l)
                     break
                 else:
                     pass2(l)
                     break
             else:
-                pass3(l)
-                break
+                 d=str.lower(input("Do you want to include special characters?\n"))
+                 if d==("yes" or "y"):
+                     pass4(l)
+                     break
+                 else:
+                     pass3(l)
+                     break
         else:
-            print("Error with your input")
-            exit(0)
+            b=str.lower(input("Do you want to include numbers?\n"))
+            if b==("yes" or "y"):
+                  c=str.lower(input("Do you want to include special characters?\n"))
+                  if c==("yes" or "y"):
+                      pass8(l)
+                      break
+                  else:
+                      pass6(l)
+                      break
+            else:
+                 c=str.lower(input("Do you want to include special characters?\n"))
+                 if c==("yes" or "y"):
+                     pass4(l)
+
+                     break
+                 else:
+                     pass7(l)
+                     break
     else:
         print("Please enter a value within the specified range")
-
-        
-
-
-
+        exit(0)
